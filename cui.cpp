f21c156c4e4497517d865dfe59cc779b89e976a1
lyj00912 <3122742760@qq.com>
2020-05-22 22:50:50 +0800

    modifided cui.cpp

--- cui.cpp
+++ cui.cpp
@@ -1,6 +1,7 @@
 #include <cstdio>
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <Windows.h>
 #include "Pipe.h"
 using namespace std;
@@ -12,6 +13,7 @@ inline void SSystem(string str){
 }
 
 void CuiInit(int argc, char** argv){
+//	cout << strcmp(argv[1], "ReadPipe");
 	//argv[0] = exe address;
 	//argv[1] = command;
 	//argv[2] = command argument 1;
@@ -21,14 +23,21 @@ void CuiInit(int argc, char** argv){
 		return;      //back to main()
 	}
 	
-	if(argv[1] == "ReadPipe"){
+	if(strcmp(argv[1], "ReadPipe") == 0){
 		cout<<"READPIPE!"<<endl;
 		char buf[100];
 		ReadPipe(argv[2], buf);         //read the pipe that named "command argument 1"
-		PostQuitMessage(buf[argv[3]]);  //read the position that is "command argument 2"
+		
+		int position;
+		stringstream ss;
+		ss.clear();
+		ss << argv[3];
+		ss >> position;
+		
+		PostQuitMessage((int)buf[position]);  //read the position that is "command argument 2"
 	}
 	
-	if(argv[1] == "WritePipe"){
+	if(strcmp(argv[1], "WritePipe") == 0){
 		cout<<"WRITEPIPE!"<<endl;
 		WritePipe(argv[2], argv[3]); //write "command argument 3" into "command argument 2" pipe
 		PostQuitMessage(0);
